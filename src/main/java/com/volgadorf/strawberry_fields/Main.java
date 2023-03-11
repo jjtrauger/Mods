a9622361fed636ebb160e6c2459b64b3b93809cf
jjtrauger <83063647+jjtrauger@users.noreply.github.com>
2023-03-10 22:58:33 -1000

    Updated creative tab function
    Updating too 1.19.3 means different way of adding things to creative tabs. In with the new.

--- src/main/java/com/volgadorf/strawberry_fields/Main.java
+++ src/main/java/com/volgadorf/strawberry_fields/Main.java
@@ -35,13 +35,13 @@ public class Main {
         modEventBus.addListener(this::commonSetup);
         MinecraftForge.EVENT_BUS.register(this);
 
-        modEventBus.addListener(this::addCreative);
+        modEventBus.addListener(this::buildContents);
     }
 
     private void commonSetup(final FMLCommonSetupEvent event) {
 
     }
-
+/**
     private void addCreative(CreativeModeTabEvent.BuildContents event) {
         if(event.getTab() == CreativeModeTabs.FOOD_AND_DRINKS) {
             event.accept(ModFoodItems.PAST_MILK);
@@ -52,6 +52,15 @@ public class Main {
             event.accept(ModFoodItems.PAST_MILK);
             event.accept(ModFoodItems.CHEEMS);
             event.accept(ModBlocks.CHEEMS_FULL);
+        }
+    }
+ **/
+    @SubscribeEvent
+    public void buildContents(CreativeModeTabEvent.BuildContents event) {
+        // Add to ingredients tab
+        if (event.getTab() == CreativeModeTabs.FOOD_AND_DRINKS) {
+            event.accept(ModFoodItems.PAST_MILK);
+            event.accept(ModBlocks.CHEEMS_FULL); // Takes in an ItemLike, assumes block has registered item
         }
     }
 
