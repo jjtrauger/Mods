49ff227c61a415fc08152649f730753ec845bf70
Volgadorf <83063647+jjtrauger@users.noreply.github.com>
2023-03-16 19:39:24 -1000

    Created actual block for cutting table

---
+++ src/main/java/com/volgadorf/strawberry_fields/block/custom/CuttingTableBlock.java
@@ -0,0 +1,70 @@
+package com.volgadorf.strawberry_fields.block.custom;
+
+import com.volgadorf.strawberry_fields.block.entity.CuttingTableBlockEntity;
+import com.volgadorf.strawberry_fields.block.entity.ModBlockEntities;
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.BaseEntityBlock;
+import net.minecraft.world.level.block.RenderShape;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityTicker;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraftforge.network.NetworkHooks;
+import org.jetbrains.annotations.Nullable;
+
+public class CuttingTableBlock extends BaseEntityBlock {
+    public CuttingTableBlock(Properties p_49224_) {
+        super(p_49224_);
+    }
+
+    /* Block Entity Stuff */
+
+    @Override
+    public RenderShape getRenderShape(BlockState p_49232_) {
+        return RenderShape.MODEL;
+    }
+
+    @Override
+    public void onRemove(BlockState pState, Level pLevel, BlockPos pPos, BlockState pNewState, boolean pIsMoving) {
+        if (pState.getBlock() != pNewState.getBlock()) {
+            BlockEntity blockEntity = pLevel.getBlockEntity(pPos);
+            if (blockEntity instanceof CuttingTableBlockEntity) {
+                ((CuttingTableBlockEntity) blockEntity).drops();
+            }
+        }
+        super.onRemove(pState, pLevel, pPos, pNewState, pIsMoving);
+    }
+
+    @Override
+    public InteractionResult use(BlockState pState, Level pLevel, BlockPos pPos,
+                                 Player pPlayer, InteractionHand pHand, BlockHitResult pHit) {
+        if (!pLevel.isClientSide()) {
+            BlockEntity entity = pLevel.getBlockEntity(pPos);
+            if(entity instanceof CuttingTableBlockEntity) {
+                NetworkHooks.openScreen(((ServerPlayer)pPlayer), (CuttingTableBlockEntity)entity, pPos);
+            } else {
+                throw new IllegalStateException("Our Container provider is missing!");
+            }
+        }
+
+        return InteractionResult.sidedSuccess(pLevel.isClientSide());
+    }
+
+    @Nullable
+    @Override
+    public BlockEntity newBlockEntity(BlockPos pos, BlockState state) {
+        return new CuttingTableBlockEntity(pos, state);
+    }
+
+    @Nullable
+    @Override
+    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level level, BlockState state, BlockEntityType<T> type) {
+        return createTickerHelper(type, ModBlockEntities.CUTTING_TABLE.get(), CuttingTableBlockEntity::tick);
+    }
+}
