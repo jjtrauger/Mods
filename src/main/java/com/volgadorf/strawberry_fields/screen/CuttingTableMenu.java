27119b7a9c4aafcfcd1fdfe7d9a4e80281cf3813
Volgadorf <83063647+jjtrauger@users.noreply.github.com>
2023-03-21 22:06:03 -1000

    Aligned menu with screen
    Also copied and commented a bunch of crafterblock methods as I am researching how they work

--- src/main/java/com/volgadorf/strawberry_fields/screen/CuttingTableMenu.java
+++ src/main/java/com/volgadorf/strawberry_fields/screen/CuttingTableMenu.java
@@ -3,11 +3,19 @@ package com.volgadorf.strawberry_fields.screen;
 import com.volgadorf.strawberry_fields.block.ModBlocks;
 import com.volgadorf.strawberry_fields.block.entity.CuttingTableBlockEntity;
 import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.network.protocol.game.ClientboundContainerSetSlotPacket;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.Container;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.player.StackedContents;
 import net.minecraft.world.inventory.*;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.CraftingRecipe;
+import net.minecraft.world.item.crafting.Recipe;
+import net.minecraft.world.item.crafting.RecipeType;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraftforge.common.capabilities.CapabilityManager;
 import net.minecraftforge.common.capabilities.CapabilityToken;
@@ -16,38 +24,42 @@ import net.minecraftforge.common.capabilities.ForgeCapabilities;
 import net.minecraftforge.items.SlotItemHandler;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Optional;
+
+
 public class CuttingTableMenu extends AbstractContainerMenu {
 
     public final CuttingTableBlockEntity blockEntity;
     private final Level level;
     private final ContainerData data;
+
     protected CuttingTableMenu(int id, Inventory inv, FriendlyByteBuf extraData) {
-    this(id, inv, inv.player.level.getBlockEntity(extraData.readBlockPos()), new SimpleContainerData(0));
+        this(id, inv, inv.player.level.getBlockEntity(extraData.readBlockPos()), new SimpleContainerData(0));
     }
 
     public CuttingTableMenu(int id, Inventory inv, BlockEntity entity, ContainerData data) {
-        super (ModMenuTypes.CUTTING_TABLE_MENU.get(), id);
+        super(ModMenuTypes.CUTTING_TABLE_MENU.get(), id);
         checkContainerSize(inv, 10);
         blockEntity = (CuttingTableBlockEntity) entity;
         this.level = inv.player.level;
         this.data = data;
-        
+
         addPlayerInventory(inv);
         addPlayerHotbar(inv);
 
         //this builds the slots; need to figure expand this to make 10 and need to test what each number means
         //in terms of position
         this.blockEntity.getCapability(ForgeCapabilities.ITEM_HANDLER).ifPresent(handler -> {
-            this.addSlot(new SlotItemHandler(handler, 0, 12, 15));
-            this.addSlot(new SlotItemHandler(handler, 1, 32, 15));
-            this.addSlot(new SlotItemHandler(handler, 2, 52, 15));
-            this.addSlot(new SlotItemHandler(handler, 3, 12, 35));
-            this.addSlot(new SlotItemHandler(handler, 4, 32, 35));
-            this.addSlot(new SlotItemHandler(handler, 5, 52, 35));
-            this.addSlot(new SlotItemHandler(handler, 6, 12, 55));
-            this.addSlot(new SlotItemHandler(handler, 7, 32, 55));
-            this.addSlot(new SlotItemHandler(handler, 8, 52, 55));
-            this.addSlot(new SlotItemHandler(handler, 9, 86, 35));
+            this.addSlot(new SlotItemHandler(handler, 0, 26, 17));
+            this.addSlot(new SlotItemHandler(handler, 1, 44, 17));
+            this.addSlot(new SlotItemHandler(handler, 2, 62, 17));
+            this.addSlot(new SlotItemHandler(handler, 3, 26, 35));
+            this.addSlot(new SlotItemHandler(handler, 4, 44, 35));
+            this.addSlot(new SlotItemHandler(handler, 5, 62, 35));
+            this.addSlot(new SlotItemHandler(handler, 6, 26, 53));
+            this.addSlot(new SlotItemHandler(handler, 7, 44, 53));
+            this.addSlot(new SlotItemHandler(handler, 8, 62, 53));
+            this.addSlot(new SlotItemHandler(handler, 9, 116, 35));
         });
 
         addDataSlots(data);
@@ -84,6 +96,7 @@ public class CuttingTableMenu extends AbstractContainerMenu {
             this.addSlot(new Slot(playerInventory, i, 8 + i * 18, 144));
         }
     }
+
 
     @Override
     public ItemStack quickMoveStack(Player playerIn, int index) {
@@ -124,15 +137,195 @@ public class CuttingTableMenu extends AbstractContainerMenu {
                 player, ModBlocks.CUTTING_TABLE.get());
     }
 
-    public boolean isCrafting(){
+    public boolean isCrafting() {
         return data.get(0) > 0;
     }
 
-    public int getScaledProgress(){
+    public int getScaledProgress() {
         int progress = this.data.get(0);
         int maxProgress = this.data.get(1);
         int progressArrowSize = 26;
 
-        return maxProgress != 0 && progress != 0? progress * progressArrowSize / maxProgress :0;
+        return maxProgress != 0 && progress != 0 ? progress * progressArrowSize / maxProgress : 0;
     }
 }
+
+    /* STUFF COPIED FROM CRAFTINGMENU
+
+    protected static void slotChangedCraftingGrid(AbstractContainerMenu p_150547_, Level p_150548_, Player p_150549_, CraftingContainer p_150550_, ResultContainer p_150551_) {
+        if (!p_150548_.isClientSide) {
+            ServerPlayer serverplayer = (ServerPlayer)p_150549_;
+            ItemStack itemstack = ItemStack.EMPTY;
+            Optional<CraftingRecipe> optional = p_150548_.getServer().getRecipeManager().getRecipeFor(RecipeType.CRAFTING, p_150550_, p_150548_);
+            if (optional.isPresent()) {
+                CraftingRecipe craftingrecipe = optional.get();
+                if (p_150551_.setRecipeUsed(p_150548_, serverplayer, craftingrecipe)) {
+                    ItemStack itemstack1 = craftingrecipe.assemble(p_150550_);
+                    if (itemstack1.isItemEnabled(p_150548_.enabledFeatures())) {
+                        itemstack = itemstack1;
+                    }
+                }
+            }
+
+            p_150551_.setItem(0, itemstack);
+            p_150547_.setRemoteSlot(0, itemstack);
+            serverplayer.connection.send(new ClientboundContainerSetSlotPacket(p_150547_.containerId, p_150547_.incrementStateId(), 0, itemstack));
+        }
+    }
+
+    public void slotsChanged(Container p_39366_) {
+        this.access.execute((p_39386_, p_39387_) -> {
+            slotChangedCraftingGrid(this, p_39386_, this.player, this.craftSlots, this.resultSlots);
+        });
+    }
+
+    public void fillCraftSlotsStackedContents(StackedContents p_39374_) {
+        this.craftSlots.fillStackedContents(p_39374_);
+    }
+
+    public void clearCraftingContent() {
+        this.craftSlots.clearContent();
+        this.resultSlots.clearContent();
+    }
+
+    public boolean recipeMatches(Recipe<? super CraftingContainer> p_39384_) {
+        return p_39384_.matches(this.craftSlots, this.player.level);
+    }
+
+    public void removed(Player p_39389_) {
+        super.removed(p_39389_);
+        this.access.execute((p_39371_, p_39372_) -> {
+            this.clearContainer(p_39389_, this.craftSlots);
+        });
+    }
+
+
+    public boolean canTakeItemForPickAll(ItemStack p_39381_, Slot p_39382_) {
+        return p_39382_.container != this.resultSlots && super.canTakeItemForPickAll(p_39381_, p_39382_);
+    }
+
+}
+
+
+public class CuttingTableMenu extends RecipeBookMenu<CraftingContainer> {
+    public static final int RESULT_SLOT = 0;
+    private static final int CRAFT_SLOT_START = 1;
+    private static final int CRAFT_SLOT_END = 10;
+    private static final int INV_SLOT_START = 10;
+    private static final int INV_SLOT_END = 37;
+    private static final int USE_ROW_SLOT_START = 37;
+    private static final int USE_ROW_SLOT_END = 46;
+    private final CraftingContainer craftSlots = new CraftingContainer(this, 3, 3);
+    private final ResultContainer resultSlots = new ResultContainer();
+    private final ContainerLevelAccess access;
+    private final Player player;
+
+    public CuttingTableMenu(int p_39353_, Inventory p_39354_) {
+        this(p_39353_, p_39354_, ContainerLevelAccess.NULL);
+    }
+
+    public CuttingTableMenu(int p_39356_, Inventory p_39357_, ContainerLevelAccess p_39358_) {
+        super(MenuType.CRAFTING, p_39356_);
+        this.access = p_39358_;
+        this.player = p_39357_.player;
+        this.addSlot(new ResultSlot(p_39357_.player, this.craftSlots, this.resultSlots, 0, 124, 35));
+
+        for(int i = 0; i < 3; ++i) {
+            for(int j = 0; j < 3; ++j) {
+                this.addSlot(new Slot(this.craftSlots, j + i * 3, 30 + j * 18, 17 + i * 18));
+            }
+        }
+
+        for(int k = 0; k < 3; ++k) {
+            for(int i1 = 0; i1 < 9; ++i1) {
+                this.addSlot(new Slot(p_39357_, i1 + k * 9 + 9, 8 + i1 * 18, 84 + k * 18));
+            }
+        }
+
+        for(int l = 0; l < 9; ++l) {
+            this.addSlot(new Slot(p_39357_, l, 8 + l * 18, 142));
+        }
+
+    }
+
+    protected static void slotChangedCraftingGrid(AbstractContainerMenu p_150547_, Level p_150548_, Player p_150549_, CraftingContainer p_150550_, ResultContainer p_150551_) {
+        if (!p_150548_.isClientSide) {
+            ServerPlayer serverplayer = (ServerPlayer)p_150549_;
+            ItemStack itemstack = ItemStack.EMPTY;
+            Optional<CraftingRecipe> optional = p_150548_.getServer().getRecipeManager().getRecipeFor(RecipeType.CRAFTING, p_150550_, p_150548_);
+            if (optional.isPresent()) {
+                CraftingRecipe craftingrecipe = optional.get();
+                if (p_150551_.setRecipeUsed(p_150548_, serverplayer, craftingrecipe)) {
+                    ItemStack itemstack1 = craftingrecipe.assemble(p_150550_);
+                    if (itemstack1.isItemEnabled(p_150548_.enabledFeatures())) {
+                        itemstack = itemstack1;
+                    }
+                }
+            }
+
+            p_150551_.setItem(0, itemstack);
+            p_150547_.setRemoteSlot(0, itemstack);
+            serverplayer.connection.send(new ClientboundContainerSetSlotPacket(p_150547_.containerId, p_150547_.incrementStateId(), 0, itemstack));
+        }
+    }
+
+    public void slotsChanged(Container p_39366_) {
+        this.access.execute((p_39386_, p_39387_) -> {
+            slotChangedCraftingGrid(this, p_39386_, this.player, this.craftSlots, this.resultSlots);
+        });
+    }
+
+    public void fillCraftSlotsStackedContents(StackedContents p_39374_) {
+        this.craftSlots.fillStackedContents(p_39374_);
+    }
+
+    public void clearCraftingContent() {
+        this.craftSlots.clearContent();
+        this.resultSlots.clearContent();
+    }
+
+    public boolean recipeMatches(Recipe<? super CraftingContainer> p_39384_) {
+        return p_39384_.matches(this.craftSlots, this.player.level);
+    }
+
+    public void removed(Player p_39389_) {
+        super.removed(p_39389_);
+        this.access.execute((p_39371_, p_39372_) -> {
+            this.clearContainer(p_39389_, this.craftSlots);
+        });
+    }
+
+    public boolean stillValid(Player p_39368_) {
+        return stillValid(this.access, p_39368_, Blocks.CRAFTING_TABLE);
+    }
+
+
+    public boolean canTakeItemForPickAll(ItemStack p_39381_, Slot p_39382_) {
+        return p_39382_.container != this.resultSlots && super.canTakeItemForPickAll(p_39381_, p_39382_);
+    }
+
+    public int getResultSlotIndex() {
+        return 0;
+    }
+
+    public int getGridWidth() {
+        return this.craftSlots.getWidth();
+    }
+
+    public int getGridHeight() {
+        return this.craftSlots.getHeight();
+    }
+
+    public int getSize() {
+        return 10;
+    }
+
+    public RecipeBookType getRecipeBookType() {
+        return RecipeBookType.CRAFTING;
+    }
+
+    public boolean shouldMoveToInventory(int p_150553_) {
+        return p_150553_ != this.getResultSlotIndex();
+    }
+}
+*/
