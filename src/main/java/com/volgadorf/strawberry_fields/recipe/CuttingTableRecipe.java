9ad303836d52b0ee62d275cb623b7c248585d1cb
Volgadorf <83063647+jjtrauger@users.noreply.github.com>
2023-03-28 19:58:50 -1000

    Initializing New Recipe Type for cutting table
    need to figure out the new getResultItem(RegistryAccess p_267052_)

---
+++ src/main/java/com/volgadorf/strawberry_fields/recipe/CuttingTableRecipe.java
@@ -0,0 +1,120 @@
+package com.volgadorf.strawberry_fields.recipe;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.volgadorf.strawberry_fields.Main;
+import com.volgadorf.strawberry_fields.block.entity.CuttingTableBlockEntity;
+import net.minecraft.core.NonNullList;
+import net.minecraft.core.RegistryAccess;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.GsonHelper;
+import net.minecraft.world.SimpleContainer;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.*;
+import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
+
+public class CuttingTableRecipe implements Recipe<SimpleContainer> {
+
+    private final ResourceLocation id;
+    private final ItemStack output;
+    private final NonNullList<Ingredient> recipeItems;
+
+    public CuttingTableRecipe(ResourceLocation id, ItemStack output,
+                                    NonNullList<Ingredient> recipeItems) {
+        this.id = id;
+        this.output = output;
+        this.recipeItems = recipeItems;
+    }
+
+
+    @Override
+    public boolean matches(SimpleContainer pContainer, Level pLevel) {
+
+        if(pLevel.isClientSide()) {
+            return false;
+        }
+        return false;
+    }
+
+    @Override
+    public ItemStack assemble(SimpleContainer pContainer, RegistryAccess p_267165_) {
+        return output;
+    }
+
+    @Override
+    public boolean canCraftInDimensions(int pWidth, int pHeight) {
+        return true;
+    }
+
+    @Override
+    public ItemStack getResultItem(RegistryAccess p_267052_) {
+        return output.copy();
+    }
+
+
+    @Override
+    public ResourceLocation getId() {
+        return id;
+    }
+
+    @Override
+    public RecipeSerializer<?> getSerializer() {
+        return Serializer.INSTANCE;
+    }
+
+    @Override
+    public RecipeType<?> getType() {
+        return Type.INSTANCE;
+    }
+
+
+    public static class Serializer implements RecipeSerializer<CuttingTableRecipe> {
+        public static final Serializer INSTANCE = new Serializer();
+        public static final ResourceLocation ID =
+                new ResourceLocation(Main.MOD_ID, "cutting_table_recipes");
+
+        @Override
+        public CuttingTableRecipe fromJson(ResourceLocation pRecipeId, JsonObject pSerializedRecipe) {
+            ItemStack output = ShapedRecipe.itemStackFromJson(GsonHelper.getAsJsonObject(pSerializedRecipe, "output"));
+
+            JsonArray ingredients = GsonHelper.getAsJsonArray(pSerializedRecipe, "ingredients");
+            NonNullList<Ingredient> inputs = NonNullList.withSize(9, Ingredient.EMPTY);
+
+            for (int i = 0; i < inputs.size(); i++) {
+                inputs.set(i, Ingredient.fromJson(ingredients.get(i)));
+            }
+
+            return new CuttingTableRecipe(pRecipeId, output, inputs);
+        }
+
+        @Override
+        public @Nullable CuttingTableRecipe fromNetwork(ResourceLocation id, FriendlyByteBuf buf) {
+            NonNullList<Ingredient> inputs = NonNullList.withSize(buf.readInt(), Ingredient.EMPTY);
+
+            for (int i = 0; i < inputs.size(); i++) {
+                inputs.set(i, Ingredient.fromNetwork(buf));
+            }
+
+            ItemStack output = buf.readItem();
+            return new CuttingTableRecipe(id, output, inputs);
+        }
+
+        @Override
+        public void toNetwork(FriendlyByteBuf buf, CuttingTableRecipe recipe) {
+            buf.writeInt(recipe.getIngredients().size());
+
+            for (Ingredient ing : recipe.getIngredients()) {
+                ing.toNetwork(buf);
+            }
+            buf.writeItemStack(recipe.getResultItem(), false);
+        }
+    }
+
+    public static class Type implements RecipeType<CuttingTableRecipe> {
+        private Type() { }
+        public static final Type INSTANCE = new Type();
+        public static final String ID = "cutting_table_recipes";
+    }
+}
---
+++ src/main/java/com/volgadorf/strawberry_fields/recipe/ModRecipes.java
@@ -0,0 +1,20 @@
+package com.volgadorf.strawberry_fields.recipe;
+
+import com.volgadorf.strawberry_fields.Main;
+import net.minecraft.world.item.crafting.RecipeSerializer;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModRecipes {
+    public static final DeferredRegister<RecipeSerializer<?>> SERIALIZERS =
+            DeferredRegister.create(ForgeRegistries.RECIPE_SERIALIZERS, Main.MOD_ID);
+
+    public static final RegistryObject<RecipeSerializer<CuttingTableRecipe>> CUTTING_TABLE_SERIALIZER =
+            SERIALIZERS.register("cutting_table_recipes", () -> CuttingTableRecipe.Serializer.INSTANCE);
+
+    public static void register(IEventBus eventBus) {
+        SERIALIZERS.register(eventBus);
+    }
+}
