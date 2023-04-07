fb11fb1fb099cf4c48c235153bc1648f50042ad3
Volgadorf <83063647+jjtrauger@users.noreply.github.com>
2023-04-07 11:18:40 -1000

    Updated Creative Tabs

--- src/main/java/com/volgadorf/strawberry_fields/item/ModCreativeModeTabs.java
+++ src/main/java/com/volgadorf/strawberry_fields/item/ModCreativeModeTabs.java
@@ -1,6 +1,7 @@
 package com.volgadorf.strawberry_fields.item;
 
 import com.volgadorf.strawberry_fields.Main;
+import com.volgadorf.strawberry_fields.block.ModBlocks;
 import com.volgadorf.strawberry_fields.block.custom.Cheems_Wheel_Block;
 import com.volgadorf.strawberry_fields.item.ModFoodItems;
 import net.minecraft.network.chat.Component;
@@ -14,13 +15,19 @@ import net.minecraftforge.fml.common.Mod;
 @Mod.EventBusSubscriber(modid = Main.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ModCreativeModeTabs {
     public static CreativeModeTab VOLG_TAB;
+    public static CreativeModeTab VOLG_TAB2;
 
     @SubscribeEvent
     public static void registerCreativeModeTabs(CreativeModeTabEvent.Register event) {
         VOLG_TAB = event.registerCreativeModeTab(new ResourceLocation(Main.MOD_ID, "food_tab"),
                 builder -> builder.icon(() -> new ItemStack(ModFoodItems.CHEEMS.get()))
                         .title(Component.translatable("creativemodetab.food_tab")));
+
+        VOLG_TAB2 = event.registerCreativeModeTab(new ResourceLocation(Main.MOD_ID, "kitchen_tools_tab"),
+                builder -> builder.icon(() -> new ItemStack(ModFoodItems.KNIFE.get()))
+                        .title(Component.translatable("creativemodetab.kitchen_tools_tab")));
     }
+
 
 
 
@@ -30,8 +37,18 @@ public class ModCreativeModeTabs {
              .icon(() -> new ItemStack(ModFoodItems.CHEEMS.get()))
              .title(Component.translatable("creativemodetab.food_tab"))
              .displayItems((params, output) -> {
-                 output.accept(ModFoodItems.CHEEMS.get());
                  output.accept(ModFoodItems.PAST_MILK.get());
+                 output.accept(ModFoodItems.CHEEMS.get());
+                 output.accept(ModBlocks.CHEEMS_FULL.get().asItem());
+             })
+     );
+
+     VOLG_TAB2 = event.registerCreativeModeTab(new ResourceLocation(Main.MOD_ID, "kitchen_tools_tab"), builder -> builder
+             .icon(() -> new ItemStack(ModFoodItems.KNIFE.get()))
+             .title(Component.translatable("creativemodetab.kitchen_tools_tab"))
+             .displayItems((params, output) -> {
+                 output.accept(ModFoodItems.KNIFE.get());
+                 output.accept(ModBlocks.CUTTING_TABLE.get().asItem());
              })
      );
  }
