87f3bbeebb825f84eb65a62dc3e7ecef7bd4e414
Volgadorf <83063647+jjtrauger@users.noreply.github.com>
2023-04-07 11:11:49 -1000

    Added KnifeItem

--- src/main/java/com/volgadorf/strawberry_fields/item/ModFoodItems.java
+++ src/main/java/com/volgadorf/strawberry_fields/item/ModFoodItems.java
@@ -1,7 +1,10 @@
 package com.volgadorf.strawberry_fields.item;
 
 import com.volgadorf.strawberry_fields.Main;
+import com.volgadorf.strawberry_fields.item.custom.KnifeItem;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Tier;
+import net.minecraft.world.item.Tiers;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -12,10 +15,13 @@ public class ModFoodItems {
             DeferredRegister.create(ForgeRegistries.ITEMS, Main.MOD_ID);
 
     public static final RegistryObject<Item> PAST_MILK = ITEMS.register("past_milk",
-            () -> new Item(new Item.Properties()));
+            () -> new Item(new Item.Properties().stacksTo(64)));
 
     public static final RegistryObject<Item> CHEEMS = ITEMS.register("cheems",
-            () -> new Item(new Item.Properties()));
+            () -> new Item(new Item.Properties().stacksTo(64)));
+    public static final RegistryObject<Item> KNIFE = ITEMS.register("knife",
+            () -> new KnifeItem(Tiers.IRON, 2, -1.4f, new Item.Properties().durability(30)));
+
 
 
     public static void register(IEventBus eventBus) {
